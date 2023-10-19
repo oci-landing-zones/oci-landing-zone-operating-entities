82e870d98471f6a566997f2192a796a50a2fdfd0
cosmindev <cosmin.tudor82@gmail.com>
2023-10-19 18:33:11 +0300

    Limit OCI TF Provider version to <= 5.16.0 to avoid the backwards compatibility issues of the NFW

--- orchestrator/orm-facade/provider.tf
+++ orchestrator/orm-facade/provider.tf
@@ -10,24 +10,14 @@ provider "oci" {
   private_key_password = var.private_key_password
 }
 
-provider "oci" {
-  alias                = "home"
-  region               = local.regions_map[local.home_region_key]
-  tenancy_ocid         = var.tenancy_ocid
-  user_ocid            = var.user_ocid
-  fingerprint          = var.fingerprint
-  private_key_path     = var.private_key_path
-  private_key_password = var.private_key_password
-}
-
 terraform {
   required_version = ">= 1.2.0, < 1.3.0"
 
   required_providers {
     oci = {
       source                = "oracle/oci"
-      version               = ">= 4.80.0"
-      configuration_aliases = [oci.home]
+      version               = "<= 5.16.0"
+      configuration_aliases = [oci]
     }
   }
 } 
