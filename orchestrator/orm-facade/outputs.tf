54a423bf4a987b638cd85ddc6eed6627754cff85
cosmindev <cosmin.tudor82@gmail.com>
2023-11-16 15:56:21 +0200

    Update to use version 0.6.0 of the networking module
    Former-commit-id: 971082444dae71d1b62dca62b36f1fb2eb6d08fb

--- orchestrator/orm-facade/outputs.tf
+++ orchestrator/orm-facade/outputs.tf
@@ -1,3 +1,12 @@
+# ####################################################################################################### #
+# Copyright (c) 2023 Oracle and/or its affiliates,  All rights reserved.                                  #
+# Licensed under the Universal Permissive License v 1.0 as shown at https: //oss.oracle.com/licenses/upl. #
+# Author: Cosmin Tudor                                                                                    #
+# Author email: cosmin.tudor@oracle.com                                                                   #
+# Last Modified: Thu Nov 16 2023                                                                          #
+# Modified by: Cosmin Tudor, email: cosmin.tudor@oracle.com                                               #
+# ####################################################################################################### #
+
 output "provisioned_identity_resources" {
   description = "Provisioned identity resources"
   value       = module.terraform-oci-open-lz.provisioned_identity_resources
