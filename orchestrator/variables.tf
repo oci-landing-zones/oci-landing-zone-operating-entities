b59add68151b5a27d5ed2a6277447191a9ddd142
Pablo Alonso <pablo.alonso@oracle.com>
2024-02-15 11:20:16 +0100

    Fixed display_name in default_security_list

--- orchestrator/variables.tf
+++ orchestrator/variables.tf
@@ -245,6 +245,7 @@ variable "network_configuration" {
           compartment_id  = optional(string),
           compartment_key = optional(string),
           defined_tags    = optional(map(string)),
+          display_name    = optional(string),
           freeform_tags   = optional(map(string)),
           
           ingress_rules = optional(list(object({
